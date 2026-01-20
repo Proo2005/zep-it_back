239b126ae87614c9dca1dec800ec5c45c1851f79
Proo2005 <prochak1922@gmail.com>
2026-01-20 20:48:58 +0530

    frotend utilized

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://zep-it-front.vercel.app/", // frontend URL
+  credentials: true, 
+}));
+app.use(express.json());
+
 app.use(express.json());
 
 // ROUTES
