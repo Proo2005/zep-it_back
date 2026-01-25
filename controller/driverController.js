6fc3f19937b0e8a6f768a9a56d9ae35e7c181689
Proo2005 <prochak1922@gmail.com>
2026-01-25 18:58:13 +0530

    driver

--- controller/driverController.js
+++ controller/driverController.js
@@ -13,9 +13,27 @@ export const getDrivers = async (req, res) => {
 /* Add new driver */
 export const addDriver = async (req, res) => {
   try {
-    const driver = await Driver.create(req.body);
+    console.log("INCOMING DRIVER:", req.body);
+
+    const driver = await Driver.create({
+      name: req.body.name,
+      phoneNumber: req.body.phoneNumber,
+      address: req.body.address,
+      city: req.body.city,
+      state: req.body.state,
+      vehicleNumber: req.body.vehicleNumber,
+      vehicleType: req.body.vehicleType,
+      rating: req.body.rating || 5,
+    });
+
     res.status(201).json(driver);
   } catch (err) {
-    res.status(400).json({ message: "Failed to add driver" });
+    console.error("DRIVER SAVE ERROR:", err);
+
+    res.status(400).json({
+      message: "Driver not saved",
+      error: err.message,
+    });
   }
 };
+
