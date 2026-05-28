c771ad71eaf5362e5d873685b3c9ec8b41417696
Prodipta Chakraborty <prochak1922@gmail.com>
2026-05-28 22:48:29 +0530

    back     payment history  in controller

--- models/Payment.js
+++ models/Payment.js
@@ -39,7 +39,7 @@ const paymentSchema = new mongoose.Schema(
 
     status: {
       type: String,
-      enum: ["success", "failed"],
+      enum: ["pending", "success", "failed"],
       default: "success",
     },
   },
